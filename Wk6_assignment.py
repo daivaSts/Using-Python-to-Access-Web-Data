4a24dc0a70105b3a5250587fb434f1eac88940de
Daiva Satas <daiva.satas@gmail.com>
2015-11-07 18:06:16 -0600

    innitial commit

---
+++ Wk6_assignment.py
@@ -0,0 +1,29 @@
+# Coursera.org /Learn to Program and Analyze Data with Python Specialization
+# Course 3 - Using Python to Access Web Data
+# Week 6
+# Assignment: Extracting Data from JSON
+'''
+The program will prompt for a URL, read the JSON data from that URL using urllib and then parse and extract the comment
+counts from the JSON data, compute the sum of the numbers in the file and enter the sum below.
+'''
+import json
+import urllib
+
+url = 'http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_169861.json'
+url_instance = urllib.urlopen(url)
+data_string = url_instance.read()
+
+try: json_dict = json.loads(str(data_string))
+except: json_dict = None
+
+json_str = json.dumps(json_dict, indent=4)
+#print json_str
+
+json_list =json_dict["comments"]
+total = 0
+
+for num in range(len(json_list)):
+    total += json_list[num]["count"]
+
+
+print "Total comments count ", total
